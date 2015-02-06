d6b7e4b3648fe75856f2e15f3bb3d4a25e935954
Dominic Charley-Roy <dominiccharleyroy@gmail.com>
2015-02-06 15:47:17 -0500

    Added support for loading multiple songs and then start/stopping songs.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 (function() {
   var dropZone = document.getElementById("drop-zone");
   var AudioContext = window.AudioContext || window.webkitAudioContext;
+  var tracks = {};
+  var trackId = 0;
 
   dropZone.addEventListener("dragover", function(e) {
     e.preventDefault();
@@ -18,7 +20,7 @@
     // Set up the reader
     var reader = new FileReader();
     reader.onload = function (result) {
-      dropZone.innerHTML = "Loading complete, processing...";
+      dropZone.innerHTML = "Drop songs here.";
       return loadSong(fileName, result.target.result);
     };
     reader.onerror = function(e) {
@@ -30,14 +32,64 @@
     return false;
   });
 
+  var trackTemplate = document.getElementById('track-template').innerHTML;
+  Mustache.parse(trackTemplate);
+
   var loadSong = function(name, data) {
     var ctx = new AudioContext();
     ctx.decodeAudioData(data, function(buffer) {
-      var source = ctx.createBufferSource();
-      source.buffer = buffer;
-      source.connect(ctx.destination);
+      // Create a new track
+      var source = setupSource(ctx, buffer);
       source.start(0);
+
+      var id = trackId++;
+      tracks[id] = {
+        context: ctx,
+        source: source,
+        buffer: buffer,
+        playing: true
+      };
+
+      // Create the element from the template
+      var rendered = Mustache.render(trackTemplate, {
+        name: name,
+        id: id
+      });
+
+      var trackDiv = document.createElement('div');
+      trackDiv.classList.add('track');
+      trackDiv.id = 'track-' + id;
+      trackDiv.innerHTML = rendered;
+      document.body.appendChild(trackDiv);
+
+      // Set up the play stop button
+      var playStopButton = document.querySelector('#track-' + id + ' .play-stop-button', trackDiv);
+      playStopButton.addEventListener('click', function(e) {
+        var track = tracks[id];
+        if (track.playing) {
+          track.playing = false;
+          playStopButton.innerHTML = "Start"
+          track.source.stop(0);
+          track.source = null;
+        } else {
+          track.playing = true;
+          playStopButton.innerHTML = "Stop"
+          track.source = setupSource(track.context, track.buffer);
+          track.source.start(0);
+        }
+        // Swallow the event
+        e.preventDefault();
+        e.stopPropagation();
+        return false;
+      });
     });
+  };
+
+  var setupSource = function(ctx, buffer) {
+    var source = ctx.createBufferSource();
+    source.buffer = buffer;
+    source.connect(ctx.destination);
+    return source;
   };
 
 }).call(this);
